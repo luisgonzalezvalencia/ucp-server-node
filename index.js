2ccd6453d59a52b0b3b3b9a6d37a62f64aa55959
LAGV <luisarturogonzalezvalencia@gmail.com>
2022-05-05 19:38:06 -0300

    Todos Add form with post

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+var todosdata = [{ id: 1, title: 'Tarea 1', completed: false },
+  { id: 2, title: 'Tarea 2', completed: false },
+  { id: 3, title: 'Tarea 3', completed: false }];
+
 //settings
 app.set('port', process.env.PORT || 3000);
 app.set('appName', 'Servidor de prueba');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/public/'));
 
 app.listen('3000', function () {
@@ -15,16 +22,16 @@ app.listen('3000', function () {
 
 //metodo get para obtener el formulario de todos
 app.get('/todos', (request, response) => {
-  const data = [{ id: 1, title: 'Tarea 1', completed: false },
-  { id: 2, title: 'Tarea 2', completed: false },
-  { id: 3, title: 'Tarea 3', completed: false }];
-  response.render('todos', { todos: data });
+  
+  response.render('todos', { todos: todosdata });
 });
 
 app.get('/datatodos', (request, response) => {
   response.send('Hello World');
 });
 
-app.post('/guardartodo', (request, response) => {
+app.post('/addtodo', (request, response) => {
   console.log(request.body);
+  todosdata.push({ id: todosdata.length + 1, title: request.body.todo, completed: false });
+  response.redirect('/todos');
 });
